8382328bb78d222be569e7b56b7727259b0576b0
Kamilah <k.muhammad95@gmail.com>
2023-01-23 12:07:23 -0600

    tests complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+
+        }
+
+        public bool IsAPalindrome(string palindrome)
+        {
+            //throw new NotImplementedException();
+            var wordReverse = "";
+
+            for(var i = palindrome.Length - 1; i >= 0; i--)
+            {
+                wordReverse += palindrome[i];
+            }
+
+            if (palindrome == wordReverse.ToLower())
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("civic", true)]
+        [InlineData("level", true)]
+        [InlineData("radar", true)]
+        [InlineData("car", false)]
+        [InlineData("lever", false)]
+        [InlineData("saloon", false)]
 
+        public void IsAPalindromeTest(string palindrome, bool expected)
+        {
+            WordSmith word = new WordSmith();
+
+            bool actual = word.IsAPalindrome(palindrome);
+
+            Assert.Equal(expected, actual);
+            
         }
     }
 }
